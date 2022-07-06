bc786addba52269a104e22431f54d89526b8d354
Ítalo Rizzo <italorizzo@outlook.com>
2022-07-06 16:56:34 -0300

    .

--- assets/scripts/jobs.js
+++ assets/scripts/jobs.js
@@ -25,7 +25,6 @@ while (cont <= qnt_imagens){
 var contIndexImg = 0
 let qnt = imagens.length -1
 while (contIndexImg <= qnt){
-
     var info = informações_texto[contIndexImg]
     let nome_album = info['nome']
     let data_album = info['data']
@@ -33,11 +32,10 @@ while (contIndexImg <= qnt){
 
     let hoverInfo = document.createElement('div')
     hoverInfo.classList.add('hoverInfo')
+    hoverInfo.id = contIndexImg+1
     let imgInfo = document.createElement('img')
     imgInfo.classList.add('imgInfo')
     imgInfo.src = imagens[contIndexImg]
-    let a = document.createElement('a')
-    a.href = 'index.html'
     let imgTextInfo = document.createElement('div')
     imgTextInfo.classList.add('imgTextInfo')
     let informaçoes = document.createElement('div')
@@ -59,10 +57,88 @@ while (contIndexImg <= qnt){
     imgTextInfo.appendChild(informaçoes)
     description.appendChild(p)
     imgTextInfo.appendChild(description)    
-    a.appendChild(imgInfo)
-    a.appendChild(imgTextInfo)
-    hoverInfo.append(a)
+    hoverInfo.appendChild(imgInfo)
+    hoverInfo.appendChild(imgTextInfo)
     corpo.appendChild(hoverInfo)
-
     contIndexImg++
 }
+var img = 0
+
+const img1 = document.getElementById('1')
+const img2 = document.getElementById('2')
+const img3 = document.getElementById('3')
+const img4 = document.getElementById('4')
+const img5 = document.getElementById('5')
+const img6 = document.getElementById('6')
+const img7 = document.getElementById('7')
+const img8 = document.getElementById('8')
+const img9 = document.getElementById('9')
+const img10 = document.getElementById('10')
+const img11 = document.getElementById('11')
+const img12 = document.getElementById('12')
+
+
+function albumPatrica(){
+
+}
+
+img1.addEventListener('click', () => {
+    img = 1
+    corpo.style.display = 'none'
+    albumPatrica()
+})
+img2.addEventListener('click', () => {
+    img = 2
+    corpo.style.display = 'none'
+    albumPatrica()
+})
+img3.addEventListener('click', () => {
+    img = 3
+    corpo.style.display = 'none'
+    albumPatrica()
+})
+img4.addEventListener('click', () => {
+    img = 4
+    corpo.style.display = 'none'
+    albumPatrica()
+})
+img5.addEventListener('click', () => {
+    img = 5
+    corpo.style.display = 'none'
+    albumPatrica()
+})
+img6.addEventListener('click', () => {
+    img = 6
+    corpo.style.display = 'none'
+    albumPatrica()
+})
+img7.addEventListener('click', () => {
+    img = 7
+    corpo.style.display = 'none'
+    albumPatrica()
+})
+img8.addEventListener('click', () => {
+    img = 8
+    corpo.style.display = 'none'
+    albumPatrica()
+})
+img9.addEventListener('click', () => {
+    img = 9
+    corpo.style.display = 'none'
+    albumPatrica()
+})
+img10.addEventListener('click', () => {
+    img = 10
+    corpo.style.display = 'none'
+    albumPatrica()
+})
+img11.addEventListener('click', () => {
+    img = 11
+    corpo.style.display = 'none'
+    albumPatrica()
+})
+img12.addEventListener('click', () => {
+    img = 12
+    corpo.style.display = 'none'
+    albumPatrica()
+})
