5dbabb0730628d1fd5a090e42da7d067ecfaf6b2
Ítalo Rizzo <italorizzo@outlook.com>
2022-07-06 17:54:17 -0300

    .

--- assets/scripts/img-transição-home.js
+++ assets/scripts/img-transição-home.js
@@ -1,16 +1,27 @@
-let time = 4100,
+let time = 5100,
 qnt = 6 // quantidade de arquivos na pasta de imagens_home
-currentImageIndex = 2,
-images = document.getElementById("img-slider")
-
+currentImageIndex = 0,
+currentImageIndex2 = 2
+var images = document.getElementById("img-slider"),
+cont = 1
+while(cont <= qnt){
+    var slider = document.createElement('img')
+    slider.classList.add('imagem-tela-grande')
+    slider.src = `image/imagens_home/imagem_${cont + 1}.jpg`
+    images.appendChild(slider)
+    cont++
+}
+const imagem_tela_grande = document.querySelectorAll('.imagem-tela-grande')
 function nextImage() {
-    images.style.backgroundImage = `url(image/imagens_home/imagem_${currentImageIndex}.jpg)`
-
+    images.style.backgroundImage = `url(image/imagens_home/imagem_${currentImageIndex2}.jpg)`
+    imagem_tela_grande[currentImageIndex].classList.remove('select')
     currentImageIndex++
 
     if(currentImageIndex >= qnt){
-        currentImageIndex = 1
+        currentImageIndex = 0
+        currentImageIndex2 = 1
     }
+    imagem_tela_grande[currentImageIndex].classList.add('select')
 }
 
 function start() {
