00bf13619f3fdfe301c61c255bd25513ba9628fc
Ítalo Rizzo <italorizzo@outlook.com>
2022-07-07 14:59:03 -0300

    .

--- assets/scripts/img-transição-home.js
+++ assets/scripts/img-transição-home.js
@@ -7,13 +7,13 @@ cont = 1
 while(cont <= qnt){
     var slider = document.createElement('img')
     slider.classList.add('imagem-tela-grande')
-    slider.src = `image/imagens_home/imagem_${cont + 1}.jpg`
+    slider.src = `https://source.unsplash.com/random/160${cont}x1200/` // mudar depois para `image/imagens_home/imagem_${cont + 1}.jpg`
     images.appendChild(slider)
     cont++
 }
 const imagem_tela_grande = document.querySelectorAll('.imagem-tela-grande')
 function nextImage() {
-    images.style.backgroundImage = `url(image/imagens_home/imagem_${currentImageIndex2}.jpg)`
+    images.style.backgroundImage = `https://source.unsplash.com/random/160${currentImageIndex2}x1200/` // mudar depois para url(image/imagens_home/imagem_${currentImageIndex2}.jpg)
     imagem_tela_grande[currentImageIndex].classList.remove('select')
     currentImageIndex++
 
--- assets/scripts/jobs.js
+++ assets/scripts/jobs.js
@@ -1,31 +1,39 @@
+
 var informações_texto = [
-    {'nome': 'Não sei 1', 'data': '12 maio 2022', 'descricao': 'estou sem ideias1'},
-    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2'},
-    {'nome': 'Não sei 3', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2'},
-    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2'},
-    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2'},
-    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2'},
-    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2'},
-    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2'},
-    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2'},
-    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2'},
-    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2'},
-    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2'}
+    {'nome': 'Não sei 1', 'data': '12 maio 2022', 'descricao': 'estou sem ideias1', 'tipo': 'casamento', 'lugar': 'São Paulo', 'quantidade de imagens': 2},
+    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2', 'tipo': 'casamento', 'lugar': 'São Paulo', 'quantidade de imagens': 2},
+    {'nome': 'Não sei 3', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2', 'tipo': 'casamento', 'lugar': 'São Paulo', 'quantidade de imagens': 2},
+    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2', 'tipo': 'casamento', 'lugar': 'São Paulo', 'quantidade de imagens': 2},
+    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2', 'tipo': 'casamento', 'lugar': 'São Paulo','quantidade de imagens': 2},
+    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2', 'tipo': 'casamento', 'lugar': 'São Paulo', 'quantidade de imagens': 2},
+    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2', 'tipo': 'casamento', 'lugar': 'São Paulo', 'quantidade de imagens': 2},
+    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2', 'tipo': 'casamento', 'lugar': 'São Paulo', 'quantidade de imagens': 2},
+    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2', 'tipo': 'casamento', 'lugar': 'São Paulo', 'quantidade de imagens': 2},
+    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2', 'tipo': 'casamento', 'lugar': 'São Paulo','quantidade de imagens': 2},
+    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2', 'tipo': 'casamento', 'lugar': 'São Paulo', 'quantidade de imagens': 2},
+    {'nome': 'Não sei 2', 'data': '24 maio 2010', 'descricao': 'estou sem ideias2', 'tipo': 'casamento', 'lugar': 'São Paulo', 'quantidade de imagens': 2
+}
 ]
 
-var qnt_imagens = 12 // quantidade de imagens na pasta (imagens-alguns-capa)
+var qnt_imagens = 12 // quantidade de pastas na pasta (imagens-albuns)
+var qnt_max_imagensporalbum = 11 //quantidade maxima que pode ter de fotos em cada album
 
 const corpo = document.getElementById('corpo')
 var cont = 1
+var prov = 0 //tirar depois
 var imagens = []
 while (cont <= qnt_imagens){
-    imagens.push(`image/imagens-albuns-capa/${cont}.jpg`)
+    if(prov > 9){
+        prov = 0
+    }
+    imagens.push(`https://source.unsplash.com/random/128${prov}x94${prov}/`) // adicionar depois `image/imagens-albuns/album${cont}/capa.jpg`
+    prov++
     cont++
 }
 var contIndexImg = 0
 let qnt = imagens.length -1
 while (contIndexImg <= qnt){
-    var info = informações_texto[contIndexImg]
+    let info = informações_texto[contIndexImg]
     let nome_album = info['nome']
     let data_album = info['data']
     let descrição_album = info['descricao']
@@ -63,6 +71,7 @@ while (contIndexImg <= qnt){
     contIndexImg++
 }
 var img = 0
+cont = 0
 
 const img1 = document.getElementById('1')
 const img2 = document.getElementById('2')
@@ -76,69 +85,126 @@ const img9 = document.getElementById('9')
 const img10 = document.getElementById('10')
 const img11 = document.getElementById('11')
 const img12 = document.getElementById('12')
+const corpo_album = document.getElementById('corpo-album')
 
+function albumPratica(){
+    let parallax = document.createElement('div')
+    parallax.classList.add('parallax')
+    let img_album = document.createElement('div')
+    img_album.classList.add('img-album')
+    img_album.style.backgroundImage = 'url(https://source.unsplash.com/random/1280x940/)' //tem que adicionar depois a variável `url(image/imagens-albuns/album${cont}/capa.jpg)`
 
-function albumPatrica(){
+    let info = informações_texto[img - 1]
+    let nome_album = info['nome']
+    let data_album = info['data']
+    let lugar_album = info['lugar']
+    let tipo_album = info['tipo']
+    let qnt_imagensnoalbum = info['quantidade de imagens']
 
+    let banner_escurecido = document.createElement('div')
+    banner_escurecido.classList.add('banner-escurecido')
+
+    let infos = document.createElement('div')
+    infos.classList.add('infos-album')
+
+    let tipo = document.createElement('span')
+    tipo.classList.add('tipo-album')
+    tipo.textContent = tipo_album
+
+    let lugar = document.createElement('span')
+    lugar.classList.add('lugar-album')
+    lugar.textContent=lugar_album
+
+    let data = document.createElement('span')
+    data.classList.add('data-album')
+    data.textContent = data_album
+
+    let titulo_main = document.createElement('h1')
+    titulo_main.classList.add('titulo-main')
+    titulo_main.textContent = nome_album
+
+    let imagensAlbum = document.createElement('div')
+    imagensAlbum.classList.add('imgAlbum')
+
+    infos.appendChild(tipo)
+    infos.appendChild(lugar)
+    infos.appendChild(data)
+    banner_escurecido.appendChild(infos)
+    banner_escurecido.appendChild(titulo_main)
+    parallax.appendChild(banner_escurecido)
+    parallax.appendChild(img_album)
+    corpo_album.appendChild(parallax)
+    corpo_album.appendChild(imagensAlbum)
+
+    while(cont <= Math.min(qnt_max_imagensporalbum, qnt_imagensnoalbum)){
+        var imgDentroAlbum = document.createElement('img')
+        if(cont == 0){
+            imgDentroAlbum.src = `image/imagens-albuns/album${img}/capa.jpg`
+        }else{
+            imgDentroAlbum.src = `https://source.unsplash.com/random` //colocar depois image/imagens-albuns/album${img}/${cont}.jpg
+        }
+        cont++
+        imagensAlbum.appendChild(imgDentroAlbum)
+    }
 }
 
 img1.addEventListener('click', () => {
     img = 1
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
 img2.addEventListener('click', () => {
     img = 2
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
 img3.addEventListener('click', () => {
     img = 3
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
 img4.addEventListener('click', () => {
     img = 4
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
 img5.addEventListener('click', () => {
     img = 5
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
 img6.addEventListener('click', () => {
     img = 6
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
 img7.addEventListener('click', () => {
     img = 7
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
 img8.addEventListener('click', () => {
     img = 8
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
 img9.addEventListener('click', () => {
     img = 9
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
 img10.addEventListener('click', () => {
     img = 10
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
 img11.addEventListener('click', () => {
     img = 11
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
 img12.addEventListener('click', () => {
     img = 12
     corpo.style.display = 'none'
-    albumPatrica()
+    albumPratica()
 })
